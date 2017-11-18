622ef46b2ab1e4d6989cabf6fe652ef9b7392f0b
SensorsIOT <andreas.spiess@arumba.com>
2017-11-18 21:25:37 +0100

    NTPtime added

---
+++ NTPtimeESP32/NTPtimeESP32.h
@@ -0,0 +1,53 @@
+/*
+
+   NTPtime for ESP32
+   This routine gets the unixtime from a NTP server and adjusts it to the time zone and the
+   Middle European summer time if requested
+
+  Author: Andreas Spiess V1.0 2016-6-28
+
+  Based on work from John Lassen: http://www.john-lassen.de/index.php/projects/esp-8266-arduino-ide-webconfig
+
+*/
+#ifndef NTPtime_h
+#define NTPtime_h
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+//#define DEBUG_ON
+
+struct strDateTime
+{
+  byte hour;
+  byte minute;
+  byte second;
+  int year;
+  byte month;
+  byte day;
+  byte dayofWeek;
+  boolean valid;
+};
+
+class NTPtime {
+  public:
+    NTPtime(String NTPtime);
+    strDateTime getNTPtime(float _timeZone, boolean _DayLightSaving);
+    void printDateTime(strDateTime _dateTime);
+    bool setSendInterval(unsigned long _sendInterval);  // in seconds
+    bool setRecvTimeout(unsigned long _recvTimeout);    // in seconds
+
+  private:
+    bool _sendPhase;
+    unsigned long _sentTime;
+    unsigned long _sendInterval;
+    unsigned long _recvTimeout;
+
+    strDateTime ConvertUnixTimestamp( unsigned long _tempTimeStamp);
+    boolean summerTime(unsigned long _timeStamp );
+    boolean daylightSavingTime(unsigned long _timeStamp);
+    unsigned long adjustTimeZone(unsigned long _timeStamp, float _timeZone, byte _DayLightSavingSaving);
+    WiFiUDP UDPNTPClient;
+};
+#endif
---
+++ NTPtimeESP32/examples/NTPtimeESP/NTPtimeESP.ino
@@ -0,0 +1,65 @@
+/*
+   This sketch shows an example of sending a reading to data.sparkfun.com once per day.
+
+   It uses the Sparkfun testing stream so the only customizing required is the WiFi SSID and password.
+
+   The Harringay Maker Space
+   License: Apache License v2
+*/
+#include <NTPtimeESP.h>
+
+
+NTPtime NTPch("ch.pool.ntp.org");   // Choose server pool as required
+char *ssid      = "";               // Set you WiFi SSID
+char *password  = "";               // Set you WiFi password
+
+/*
+ * The structure contains following fields:
+ * struct strDateTime
+{
+  byte hour;
+  byte minute;
+  byte second;
+  int year;
+  byte month;
+  byte day;
+  byte dayofWeek;
+  boolean valid;
+};
+ */
+strDateTime dateTime;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booted");
+  Serial.println("Connecting to Wi-Fi");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin (ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+
+  // first parameter: Time zone in floating point (for India); second parameter: 1 for European summer time; 2 for US daylight saving time (contributed by viewwer, not tested by me)
+  dateTime = NTPch.getNTPtime(1.0, 1);
+  
+  // check dateTime.valid before using the returned time
+  // Use "setSendInterval" or "setRecvTimeout" if required
+  if(dateTime.valid){
+    NTPch.printDateTime(dateTime);
+
+    byte actualHour = dateTime.hour;
+    byte actualMinute = dateTime.minute;
+    byte actualsecond = dateTime.second;
+    int actualyear = dateTime.year;
+    byte actualMonth = dateTime.month;
+    byte actualday =dateTime.day;
+    byte actualdayofWeek = dateTime.dayofWeek;
+  }
+}
